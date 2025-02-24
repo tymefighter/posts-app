59ffc717c37809e053c01c6c949ceb828cef16d1
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2025-02-24 22:47:04 +0530

    [GH-00] fix: Fixes issues

---
+++ kubernetes/mysql/start-mysql.sh
@@ -0,0 +1,3 @@
+# Create mysql headless service and stateful set
+kubectl create -f mysql-headless-service.yml
+kubectl create -f mysql-stateful-set.yml
--- start.sh
+++ start.sh
@@ -2,3 +2,8 @@
 cd kubernetes/etcd
 ./start-etcd.sh
 cd ../../
+
+# Start mysql
+cd kubernetes/mysql
+./start-mysql.sh
+cd ../../
